698fc0f64b883223d24a378201d754c04c5939c5
Leeseunghwan7305 <sun1238799@naver.com>
2022-05-05 22:54:17 +0900

    [add] recoil refactoring

--- src/component/ToDoList/TodoList.tsx
+++ src/component/ToDoList/TodoList.tsx
@@ -1,24 +1,29 @@
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState, useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector } from "../atoms";
 import CreateToDo from "../CreateToDo/CreateToDo";
-import Todo from "./Todo";
-
+import ToDo from "./Todo";
 //key 내부적으로 atom을 식별하는데 사용되는 고유한 문자열
 //시스템에서 아톰들을 구분할때 변수명 x 키값 으로함
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <div>
-      <h1>To Dos</h1>
+      <h1>To Do</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo></CreateToDo>
-      <ul>
-        {toDos.map((toDo) => (
-          <Todo key={toDo.id} {...toDo}></Todo>
-        ))}
-      </ul>
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }
